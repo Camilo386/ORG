4b71330fdb995fe672f6d6db4ee068f1cedeeb09
Camilo <juancamilo-13@hotmail.com>
2023-05-18 11:40:38 -0400

    creating formularios

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,15 +1,25 @@
 import ListaOpciones from "../ListaOpciones";
 import CampoTexto from "../CampoTexto";
+import Boton from "../Boton";
 import "./Formulario.css"
 
 const Formulario = () => {
+
+    const manejarEnvio = (evento) => {
+        evento.preventDefault();
+        console.log("Manejar el envío",evento)
+    }
+
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
             <CampoTexto titulo="Nombre" placeholder="Ingresar el nombre"/>
             <CampoTexto titulo='Puesto' placeholder="Ingrese el puesto"/>
             <CampoTexto titulo='Foto' placeholder="Ingresar enlace de foto"/>
             <ListaOpciones/>
+            <Boton>
+                Crear
+            </Boton>
         </form>
     </section>
 }
